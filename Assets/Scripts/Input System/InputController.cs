8bb73285491fff09a40c00b7134fe6bba4258f22
Logan White <lsw3961@g.rit.edu>
2021-08-22 16:18:25 -0400

    All Movement is Done

---
+++ Assets/Scripts/Input System/InputController.cs
@@ -0,0 +1,270 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input System/InputController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputController : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputController"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""ff79e48f-7247-4d4b-af27-fe21b7b853b8"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""c7ea9f0a-7a61-44f1-8b81-74897ca2593c"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Value"",
+                    ""id"": ""61972963-fefd-4eff-a182-984a7f5edc66"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Shoot"",
+                    ""type"": ""Button"",
+                    ""id"": ""888d4b8c-b4b3-4aa0-ad7b-3848ef2dd5da"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Point"",
+                    ""type"": ""Value"",
+                    ""id"": ""c952f39c-c230-4b40-9c99-15cd35319828"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""a6cbd34e-8df6-4feb-9497-a6d9df901c7f"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""422b8947-0760-4574-b5b1-a62e7a45b1c1"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""1e57cdda-aaeb-49d2-ab3f-822c755abbce"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""dda5c208-465f-402e-9c26-4e1378b98318"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""03844f5a-f3d6-429c-8b2f-44beca1609d3"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""39a98bc8-1583-42ee-8715-1b42a7c003c5"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Shoot"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""25fa4fc7-44b3-43b9-ba6d-4c0368ce0735"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Point"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4427c994-49cb-481c-b275-a3f280368b9c"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Shoot = m_Player.FindAction("Shoot", throwIfNotFound: true);
+        m_Player_Point = m_Player.FindAction("Point", throwIfNotFound: true);
+        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Shoot;
+    private readonly InputAction m_Player_Point;
+    private readonly InputAction m_Player_Dash;
+    public struct PlayerActions
+    {
+        private @InputController m_Wrapper;
+        public PlayerActions(@InputController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Shoot => m_Wrapper.m_Player_Shoot;
+        public InputAction @Point => m_Wrapper.m_Player_Point;
+        public InputAction @Dash => m_Wrapper.m_Player_Dash;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Shoot.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Shoot.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Shoot.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Point.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPoint;
+                @Point.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPoint;
+                @Point.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPoint;
+                @Dash.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Shoot.started += instance.OnShoot;
+                @Shoot.performed += instance.OnShoot;
+                @Shoot.canceled += instance.OnShoot;
+                @Point.started += instance.OnPoint;
+                @Point.performed += instance.OnPoint;
+                @Point.canceled += instance.OnPoint;
+                @Dash.started += instance.OnDash;
+                @Dash.performed += instance.OnDash;
+                @Dash.canceled += instance.OnDash;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnShoot(InputAction.CallbackContext context);
+        void OnPoint(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //basic variables
+    private Rigidbody2D rb;
+    public InputReader reader;
+    //move variables
+    private Vector2 dir = Vector2.zero;
+    private Vector2 lastDirection = Vector2.zero;
+
+    [SerializeField] private float speed = 5;
+
+    //jump variables
+    [SerializeField] private float jumpForce = 5;
+    [SerializeField] private float fallMultiplier = 5;
+    [SerializeField] private float lowJumpMultiplier = 5;
+    [SerializeField] private float slideSpeed = 1;
+    [SerializeField] private float wallJumpTime = 1;
+    [SerializeField] private float xWallForce = 1;
+    [SerializeField] private float yWallForce = 3;
+    private bool isJumping = false;
+    private bool isJumpingReleased = true;
+    private bool onGround = true;
+    private bool onWall = false;
+    private bool wallJumping = false;
+    private bool wallSliding = false;
+    [SerializeField] private Vector2 bottomOffset = Vector2.zero;
+    [SerializeField] private Vector2 leftOffset = Vector2.zero;
+    [SerializeField] private Vector2 rightOffset = Vector2.zero;
+    [SerializeField] private float overlapRadius = 2;
+    [SerializeField] private LayerMask groundedLayer = 0;
+
+
+    //dash variables
+    [SerializeField] private float dashStartTime = 2f;
+    [SerializeField] private float dashSpeed = 5f;
+    private float dashTime = 0f;
+    private bool isDashing = false;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    #region Enable & Disable
+    private void OnEnable()
+    {
+        reader.MoveEvent += Move;
+        reader.JumpEvent += Jump;
+        reader.DashEvent += DashTrigger;
+
+
+    }
+
+    private void OnDisable()
+    {
+        reader.MoveEvent -= Move;
+        reader.JumpEvent -= Jump;
+        reader.DashEvent -= DashTrigger;
+
+    }
+
+    #endregion
+
+    private void Update()
+    {
+        Walk(dir);
+        Dash();
+        IsGrounded();
+        FixJump();
+        DashTimeCounter();
+        if (wallJumping)
+        {
+            Invoke("SetWallJumpingToFalse",wallJumpTime);
+            rb.velocity = new Vector2(xWallForce * -lastDirection.x, yWallForce);
+
+        }
+        if (onWall && !onGround)
+        {
+            WallSlide();
+        }
+
+    }
+
+    /// <summary>
+    /// these methods are the movement methods and deal with how the player moves
+    /// </summary>
+    /// <param name="direction"></param>
+    private void Move(Vector2 direction)
+    {
+        dir = direction;
+        if (dir != Vector2.zero)
+        {
+            lastDirection = dir;
+
+        }
+    }
+    private void Walk(Vector2 dir)
+    {
+        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+    }
+
+    /// <summary>
+    /// these methods for checking jumping, 1 to read from the input reader, 1 to release from the input reader, and 1 to do the actualy calcuations 
+    /// </summary>
+    private void Jump()
+    {
+        isJumping = true;
+
+        if (wallSliding)
+        {
+            wallJumping = true;
+        }
+        isJumpingReleased = false;
+        reader.JumpReleaseEvent += JumpReleased;
+
+    }
+    private void JumpReleased()
+    {
+        isJumpingReleased = true;
+        reader.JumpReleaseEvent -= JumpReleased;
+    }
+    private void FixJump()
+    {
+
+        if (isJumping&&onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.velocity += Vector2.up * jumpForce;
+        }
+            if (rb.velocity.y < 0)
+            {
+
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            }
+            else if (rb.velocity.y > 0 && isJumpingReleased )
+            {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            }
+            isJumping = false;
+        
+
+    }
+    private void IsGrounded()
+    {
+        if (Physics2D.OverlapCircle((Vector2)this.transform.position + bottomOffset, overlapRadius, groundedLayer))
+        {
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
+        }
+
+        if (Physics2D.OverlapCircle((Vector2)this.transform.position + leftOffset, overlapRadius, groundedLayer) || Physics2D.OverlapCircle((Vector2)this.transform.position + rightOffset, overlapRadius, groundedLayer))
+        {
+            onWall = true;
+            wallSliding = true;
+        }
+        else
+        {
+            wallSliding = false;
+            onWall = false;
+        }
+    }
+    private void WallSlide()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, -slideSpeed);
+    }
+    private void SetWallJumpingToFalse()
+    {
+        wallJumping = false;
+    }
+
+    /// <summary>
+    /// these methods have to do with the dash and how the dash functions
+    /// </summary>
+    private void DashTrigger()
+    {
+        isDashing = true;
+    }
+    private void Dash()
+    {
+        if (dashTime < 0 && isDashing)
+        {
+            if (lastDirection.x == 0)
+            {
+                lastDirection.x = 1;
+            }
+            dashTime = dashStartTime;
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.velocity += new Vector2(lastDirection.x * dashSpeed, 0);
+            isDashing = false;
+        }
+    }
+    private void DashTimeCounter()
+    {
+        dashTime -= Time.deltaTime;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere((Vector2)this.transform.position + bottomOffset, overlapRadius);
+        Gizmos.DrawWireSphere((Vector2)this.transform.position + leftOffset, overlapRadius);
+        Gizmos.DrawWireSphere((Vector2)this.transform.position + rightOffset, overlapRadius);
+    }
+
+
+}
