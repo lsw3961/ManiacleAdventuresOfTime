f06d5fca41af7ca5dae125eb799030c2f87ad971
Logan White <lsw3961@g.rit.edu>
2021-08-29 15:09:35 -0400

    Some Boss Behavior stuff, and some work on level 1

---
+++ Assets/Scripts/Enemy/Boss/BossBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossBehavior : MonoBehaviour
+{
+    public int health = 100;
+    public int Damage = 1;
+    public PlayerLife player;
+    private float shotTime = 1.5f;
+    public Slider slider;
+
+    public void FixedUpdate()
+    {
+        if (shotTime >0)
+        {
+            shotTime -= Time.deltaTime;
+        }
+        slider.value = health;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.Damage();
+        }
+        else if (collision.tag == "Ammo")
+        {
+            health--;
+        }
+    }
+
+}
---
+++ Assets/idleBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class idleBehavior : StateMachineBehaviour
+{
+    public float timer = 1.5f;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (timer <= 0)
+        {
+            int rand = Random.Range(1, 2);
+            if (rand == 0)
+            {
+                animator.SetTrigger("attack2");
+            }
+            else
+            {
+                animator.SetTrigger("attack1");
+            }
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+}
